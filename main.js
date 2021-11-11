41e22ab9270946ba0b670676b12a9629399a80cc
seungwan Ham <seungwan7511@gmail.com>
2021-11-11 11:13:44 +0900

    21.11.11

--- main.js
+++ main.js
@@ -30,3 +30,21 @@ workBtnContainer.addEventListener('click', (event) => {
     projectContainer.classList.remove('anim-out');
   }, 300);
 });
+
+// function openPopup() {
+//   var _width = '650';
+//   var _height = '380';
+
+//   // 팝업을 가운데 위치시키기 위해 아래와 같이 값 구하기
+//   var _left = Math.ceil(( window.screen.width - _width )/2);
+//   var _top = Math.ceil(( window.screen.height - _height )/2); 
+
+//   window.open('/url', 'popup-test', 'width='+ _width +', height='+ _height +', left=' + _left + ', top='+ _top )
+// };
+
+// const content = document.querySelector('.contents');
+// const popup = document.querySelector('.popup');
+
+// content.addEventListener(('click'), () => {
+//   popup.fadeIn();
+// });
