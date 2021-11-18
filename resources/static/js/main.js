e832367ffc2e49f0080972967a1d5915ab1b3852
seunwanHam <seungwan7511@gmail.com>
2021-11-18 20:41:19 +0900

    arrow btn add + js 위치 변경

--- resources/static/js/main.js
+++ resources/static/js/main.js
@@ -29,33 +29,22 @@ navbarToggleBtn.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
 });
 
-
-// Projects
-const workBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (event) => {
-  const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
-  if (filter == null) {
-    return;
+// show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=> {
+  if(window.scrollY > navbarHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
   }
-  // Remove selection from the previous item and select the new one
-  const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
-  const target =
-    event.target.nodeName === 'BUTTON' ? event.target : event.target.parentNode;
-  target.classList.add('selected');
-
-  projectContainer.classList.add('anim-out');
-  setTimeout(() => {
-    projects.forEach((project) => {
-      console.log(project.dataset.type);
-      if (filter === '*' || filter === project.dataset.type) {
-        project.classList.remove('invisible');
-      } else {
-        project.classList.add('invisible');
-      }
-    });
-    projectContainer.classList.remove('anim-out');
-  }, 300);
 });
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', ()=> {
+  scrollIntoView('.section');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+}
